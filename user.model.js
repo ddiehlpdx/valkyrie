dbb7adacc5e1010d78cc16c7ccbbe98ba4efad06
jjenkins88 <jjenkinspdxcode@gmail.com>
2022-12-29 12:31:00 -0800

    Created User class model

---
+++ user.model.js
@@ -0,0 +1,83 @@
+import { Sequelize, DataTypes, Model } from 'sequelize';
+import bcrypt from 'bcrypt';
+
+const sequelize = new Sequelize('sqlite::memory:');
+
+class User extends Model { }
+
+User.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    firstName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    isActive: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true
+    },
+    isAdming: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    }
+}, {
+    hooks: {
+        beforeCreate: (user) => {
+            if (user.password) {
+                const salt = bcrypt.genSaltSync(10, 'a');
+                user.password = bcrypt.hashSync(user.password, salt);
+            }
+        },
+        beforeUpdate: (user) => {
+            if (user.password) {
+                const salt = bcrypt.genSaltSync(10, 'a');
+                user.password = bcrypt.hashSync(user.password, salt);
+            }
+        }
+    },
+    defaultScope: {
+        attributes: {
+            exclude: [
+                'id',
+                'username',
+                'email'
+            ]
+        }
+    },
+    scopes: {
+        auth: {
+            attributes: {
+                include: [
+                    'id',
+                    'username',
+                    'email',
+                    'password',
+                    'isActive'
+                ]
+            }
+        }
+    },
+    sequelize,
+    modelName: 'User'
+});
+
