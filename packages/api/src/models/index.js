67f64619c1bdfcfbf850f554494d9696416512ee
ddiehlpdx <ddiehl.pdx@gmail.com>
2023-02-06 16:44:12 -0800

    misc project setup.

--- packages/api/src/models/index.js
+++ packages/api/src/models/index.js
@@ -1,1 +1,3 @@
-// TODO
+const db = {};
+
+export default db;
--- packages/api/src/server.js
+++ packages/api/src/server.js
@@ -1,1 +1,16 @@
-// TODO
+import express from 'express';
+import cors from 'cors';
+import db from '#models';
+import jwt from 'jsonwebtoken';
+
+const app = express();
+
+app.use(cors()); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'API Connected to database successfully.'
+  });
+});
