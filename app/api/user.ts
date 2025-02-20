c1053765c95e2481edefd6cd002f66865b033081
ddiehlpdx <ddiehl.pdx@gmail.com>
2025-02-19 19:27:32 -0800

    user api.

---
+++ app/api/user.ts
@@ -0,0 +1,25 @@
+import { db } from "~/db.server";
+
+export async function getUserById(id: string) {
+  return db.user.findUnique({ where: { id } });
+}
+
+export async function getUserByEmail(email: string) {
+  return db.user.findUnique({ where: { email } });
+}
+
+export async function getUserByUsername(username: string) {
+  return db.user.findUnique({ where: { username } });
+}
+
+export async function signUp(email: string, username: string, password: string) {
+  return db.user.signUp(email, username, password);
+}
+
+export async function signIn(emailOrUsername: string, password: string) {
+  return db.user.signIn(emailOrUsername, password);
+}
+
+export async function usernameIsAavailable(username: string) {
+  return db.user.usernameIsAavailable(username);
+}
