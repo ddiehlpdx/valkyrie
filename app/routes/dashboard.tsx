4b21486d59fc6144afa5b28ea44077c37cf888e2
ddiehlpdx <ddiehl.pdx@gmail.com>
2025-02-27 15:46:00 -0800

    add sidebar to dashboard.

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,6 +1,23 @@
+import { AppSidebar } from "~/components/dashboard/app-sidebar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "~/components/ui/breadcrumb";
+import { Separator } from "~/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "~/components/ui/sidebar";
 import { LoaderFunctionArgs, redirect } from "@remix-run/node";
 import { Outlet } from "@remix-run/react";
 import { getSession } from "~/session.server";
+import { getUserById } from "~/api/user";
+import { getProfileByUserId } from "~/api/profile";
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const session = await getSession(request.headers.get('Cookie'));
@@ -8,14 +25,55 @@ export async function loader({ request }: LoaderFunctionArgs) {
   if (!session.has('userId')) {
     return redirect('/auth/sign-in');
   }
+  
+  const user = await getUserById(session.get('userId') as string);
+  const profile = await getProfileByUserId(user?.id as string);
 
-  return null;
+  return {
+    user,
+    profile
+  };
 }
 
 export default function Dashboard() {
   return (
       <div id="dashboard" className="flex h-screen items-center justify-center">
-          <Outlet />
+        <SidebarProvider>
+          <AppSidebar />
+          <SidebarInset>
+            <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+              <div className="flex items-center gap-2 px-4">
+                <SidebarTrigger className="-ml-1" />
+                <Separator
+                  orientation="vertical"
+                  className="mr-2 data-[orientation=vertical]:h-4"
+                />
+                <Breadcrumb>
+                  <BreadcrumbList>
+                    <BreadcrumbItem className="hidden md:block">
+                      <BreadcrumbLink href="#">
+                        Building Your Application
+                      </BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator className="hidden md:block" />
+                    <BreadcrumbItem>
+                      <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                    </BreadcrumbItem>
+                  </BreadcrumbList>
+                </Breadcrumb>
+              </div>
+            </header>
+            <Outlet />
+            <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+              <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                <div className="bg-muted/50 aspect-video rounded-xl" />
+                <div className="bg-muted/50 aspect-video rounded-xl" />
+                <div className="bg-muted/50 aspect-video rounded-xl" />
+              </div>
+              <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+            </div>
+          </SidebarInset>
+        </SidebarProvider>
       </div>
   );
 }
