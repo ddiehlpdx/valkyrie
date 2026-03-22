ff0c673ef2f28630e98bea6141c5482381627e61
jjenkins88 <jjenkinspdxcode@gmail.com>
2026-03-22 16:36:36 -0700

    Stat & Element Schema/API

---
+++ app/api/element.ts
@@ -0,0 +1,34 @@
+import { db } from "~/db.server";
+
+export async function getElementsByProjectId(projectId: string) {
+    return db.element.findMany({
+        where: { projectId },
+        orderBy: { displayOrder: 'asc' }
+    });
+}
+
+export async function createElement(data: {
+    name: string;
+    description?: string;
+    color: string;
+    iconKey: string;
+    projectId: string;
+}) {
+    return db.element.create({ data: { ...data, description: data.description || null } });
+}
+
+export async function updateElement(elementId: string, data: {
+    name?: string,
+    description?: string,
+    color?: string,
+    iconKey?: string,
+}) {
+    return db.element.update({
+        where: { id: elementId },
+        data
+    });
+}
+
+export async function deleteElement(elementId: string) {
+    return db.element.delete({ where: { id: elementId } });
+}
